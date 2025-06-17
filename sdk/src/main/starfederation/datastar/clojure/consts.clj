a544c3ef8c54f167c42efa8268c30978590e5d32
Delaney <delaneygillilan@gmail.com>
2025-06-17 07:29:15 -0700

    SDK 5 functions down to 3 (#935)
    * remove signals removed from SDK
    
    * Remove 'RemoveFragments' event type and related documentation across multiple SDKs
    
    * Add convenience function WithMergeRemove for removing elements in merge mode
    
    * Refactor FragmentMergeMode and update documentation
    
    - Updated FragmentMergeMode constants to improve clarity and consistency across SDKs.
    - Changed default merge mode from "morph" to "outer" to enhance performance and state preservation.
    - Enhanced documentation for each merge mode to explain behavior and differences between morphing and non-morphing modes.
    - Adjusted README and SDK documentation to reflect changes in FragmentMergeMode and default settings.
    
    * Remove 'Replace' in FragmentMergeMode across multiple languages and update related documentation
    
    * remove idiomorph call out
    
    * update Go SDK to match new ADR
    
    * Add elements-sugar and elements files with merge functionality and tests
    
    * remove executeScript from ADR
    
    * Add retryDuration to Go SDK
    
    * missing helper and merge options
    
    * add replace back into the spec
    
    * patch prefix
    
    * cleanup
    
    * cleanup per SDK authors
    
    * complete example
    
    * refactor: simplify example data structure in README
    
    ---------
    
    Co-authored-by: Ben Croker <57572400+bencroker@users.noreply.github.com>

--- sdk/src/main/starfederation/datastar/clojure/consts.clj
+++ sdk/src/main/starfederation/datastar/clojure/consts.clj
@@ -32,11 +32,10 @@
 ;; -----------------------------------------------------------------------------
 (def selector-dataline-literal "selector ")
 (def merge-mode-dataline-literal "mergeMode ")
-(def fragments-dataline-literal "fragments ")
+(def elements-dataline-literal "elements ")
 (def use-view-transition-dataline-literal "useViewTransition ")
 (def signals-dataline-literal "signals ")
 (def only-if-missing-dataline-literal "onlyIfMissing ")
-(def paths-dataline-literal "paths ")
 (def script-dataline-literal "script ")
 (def attributes-dataline-literal "attributes ")
 (def auto-remove-dataline-literal "autoRemove ")
@@ -45,8 +44,8 @@
 ;; -----------------------------------------------------------------------------
 ;; Default booleans
 ;; -----------------------------------------------------------------------------
-(def default-fragments-use-view-transitions
-  "Should fragments be merged using the ViewTransition API?"
+(def default-elements-use-view-transitions
+  "Should elements be merged using the ViewTransition API?"
   false)
 
 (def default-merge-signals-only-if-missing
@@ -62,64 +61,52 @@
 ;; -----------------------------------------------------------------------------
 ;; Enums
 ;; -----------------------------------------------------------------------------
-;; FragmentMergeMode
+;; ElementMergeMode
 
-(def fragment-merge-mode-morph
-  "Morphs the fragment into the existing element using idiomorph."
-  "morph")
-
-(def fragment-merge-mode-inner
-  "Replaces the inner HTML of the existing element."
-  "inner")
-
-(def fragment-merge-mode-outer
-  "Replaces the outer HTML of the existing element."
+(def element-merge-mode-outer
+  "Morphs the element into the existing element using Datastar's morphing, preserving focus and minimizing element changes."
   "outer")
 
-(def fragment-merge-mode-prepend
-  "Prepends the fragment to the existing element."
+(def element-merge-mode-inner
+  "Morphs the element into the innerHTML using Datastar's morphing, preserving focus and minimizing element changes."
+  "inner")
+
+(def element-merge-mode-remove
+  "Removes the existing element from the DOM."
+  "remove")
+
+(def element-merge-mode-prepend
+  "Prepends the element inside the existing element."
   "prepend")
 
-(def fragment-merge-mode-append
-  "Appends the fragment to the existing element."
+(def element-merge-mode-append
+  "Appends the element inside the existing element."
   "append")
 
-(def fragment-merge-mode-before
-  "Inserts the fragment before the existing element."
+(def element-merge-mode-before
+  "Inserts the element before the existing element."
   "before")
 
-(def fragment-merge-mode-after
-  "Inserts the fragment after the existing element."
+(def element-merge-mode-after
+  "Inserts the element after the existing element."
   "after")
 
-(def fragment-merge-mode-upsert-attributes
-  "Upserts the attributes of the existing element."
-  "upsertAttributes")
 
-
-(def default-fragment-merge-mode
-  "Default value for FragmentMergeMode.
-  Morphs the fragment into the existing element using idiomorph."
-  fragment-merge-mode-morph)
+(def default-element-merge-mode
+  "Default value for ElementMergeMode.
+  Morphs the element into the existing element using Datastar's morphing, preserving focus and minimizing element changes."
+  element-merge-mode-outer)
 
 
 ;; EventType
 
-(def event-type-merge-fragments
-  "An event for merging HTML fragments into the DOM."
-  "datastar-merge-fragments")
+(def event-type-merge-elements
+  "An event for merging HTML elements into the DOM."
+  "datastar-merge-elements")
 
 (def event-type-merge-signals
   "An event for merging signals."
   "datastar-merge-signals")
-
-(def event-type-remove-fragments
-  "An event for removing HTML fragments from the DOM."
-  "datastar-remove-fragments")
-
-(def event-type-remove-signals
-  "An event for removing signals."
-  "datastar-remove-signals")
 
 (def event-type-execute-script
   "An event for executing <script/> elements in the browser."
